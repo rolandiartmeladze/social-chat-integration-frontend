2fc4101e776fc4a6f7769879ec025f1cfffc7256
Roland Artmeladze <rartmeladze@gmail.com>
2025-05-22 07:00:29 -0700

    Enhance index.ts by adding comments, reordering imports, and improving code structure

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,9 +1,23 @@
-import express from 'express';
+// /backend/api/index.ts
+
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
 import messangerRouter from '../routes/messenger';
 
+dotenv.config();
+
 const app = express();
+app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 
 app.use('/messenger', messangerRouter);
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Hello World From Server!");
+});
 
 export default app;
