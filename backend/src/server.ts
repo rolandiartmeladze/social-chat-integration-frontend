2ee0043561ffa9411ca87d9cea63c3dfb9e1c8c7
Roland Artmeladze <rartmeladze@gmail.com>
2025-05-22 07:00:22 -0700

    Refactor server setup by removing unused imports and middleware, and streamline the listening port configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,25 +1,7 @@
-import express, { Request, Response } from 'express';
-import dotenv from 'dotenv';
+import app from './api/index';
 
-import cors from 'cors';
+const PORT = process.env.PORT || 5000;
 
-import messangerRouter from './routes/messenger';
-
-dotenv.config();
-
-const bodyParser = require('body-parser');
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.json());
-
-app.use('/messenger', messangerRouter);
-
-app.get("/", (_req: Request, res: Response) => {
-  res.send("Hello World From Server!");
-});
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Webhook is listening', process.env.PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
