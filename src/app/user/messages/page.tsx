fe573bff1ba5f501c186dd90af9161561c86c0ec
Roland Artmeladze <rartmeladze@gmail.com>
2025-05-23 14:13:25 -0700

    Add TelegramChat component to MessagesPage

--- src/app/user/messages/page.tsx
+++ src/app/user/messages/page.tsx
@@ -1,5 +1,7 @@
 import InstagramChat from '@/component/instagramChat/instagram-chat';
 import MessengerChat from '../../../component/messangerChat/messenger-chat';
+import TelegramChat from '@/component/telegramChat/telegramChat';
+
 export default function MessagesPage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
@@ -7,6 +9,7 @@ export default function MessagesPage() {
 <div className="flex flex-rows-2 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <MessengerChat />
       <InstagramChat />
+      <TelegramChat />
       </div>
     </div>
   );
