0a6fc2da7b328f29899dfd36919b49d1c12c8f98
Roland Artmeladze <rartmeladze@gmail.com>
2025-05-23 09:31:18 -0700

    Add InstagramChat component to display messages from backend

--- src/app/user/messages/page.tsx
+++ src/app/user/messages/page.tsx
@@ -1,11 +1,13 @@
+import InstagramChat from '@/component/instagramChat/instagram-chat';
 import MessengerChat from '../../../component/messangerChat/messenger-chat';
 export default function MessagesPage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       MessagesPage
-      <>
+<div className="flex flex-rows-2 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <MessengerChat />
-      </>
+      <InstagramChat />
+      </div>
     </div>
   );
 }
