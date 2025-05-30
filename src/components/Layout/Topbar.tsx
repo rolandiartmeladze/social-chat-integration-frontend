d2b55430577d84ed091b3f56ac9cb73c848fb39a
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-05-30 22:03:03 +0400

    feat: add Topbar component for application header

---
+++ src/components/Layout/Topbar.tsx
@@ -0,0 +1,12 @@
+export default function Topbar() {
+    return (
+        <div className="flex items-top w-full bg-sidebar-primary">
+            <h1 className="text-xl font-bold">Topbar</h1>
+            <nav>
+                <ul className="flex space-x-4">
+                    content
+                </ul>
+            </nav>
+        </div>
+    );
+};
