4864a77529b2feaf7f26fec9a9e7dcd445f9b1c9
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-05-30 22:02:38 +0400

    feat: create MainLayout component for main application layout structure

---
+++ src/components/Layout/MainLayout.tsx
@@ -0,0 +1,26 @@
+import SidebarWrapper from "@/components/Layout/SidebarWrapper";
+// import { SidebarProvider } from "@/components/ui/sidebar";
+// import Sidebar from "@/components/Layout/SidebarWrapper";
+import Topbar from "@/components/Layout/Topbar";
+// import InboxContainer from "../Inbox/InboxContainer";
+// import ChatWindow from "../Chat/ChatWindow";
+// import DetailsPanel from "../DetailsPanel/DetailsPanel";
+
+export default function MainLayout() {
+  return (
+    <div className="flex h-screen bg-background text-foreground">
+              {/* <SidebarProvider> */}
+      <SidebarWrapper />
+      <div className="flex flex-col flex-1">
+         <Topbar />
+       {/* <div className="flex flex-1 overflow-hidden">
+          <InboxContainer />
+          <ChatWindow />
+          <DetailsPanel />
+        </div> */}
+        this is the main content area
+      </div>
+      {/* </SidebarProvider> */}
+    </div>
+  );
+}
