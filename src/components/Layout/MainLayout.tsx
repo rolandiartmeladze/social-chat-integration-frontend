4e05d50a21c8e75a5ecd0ea686c5a73d21804211
Roland Artmeladze <rartmeladze@gmail.com>
2025-05-31 02:54:14 -0700

    fix: refactor MainLayout to restore SidebarProvider and improve layout structure

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -1,26 +1,28 @@
 import SidebarWrapper from "@/components/Layout/SidebarWrapper";
-// import { SidebarProvider } from "@/components/ui/sidebar";
-// import Sidebar from "@/components/Layout/SidebarWrapper";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import Topbar from "@/components/Layout/Topbar";
 import InboxContainer from "../Inbox/InboxContainer";
-// import ChatWindow from "../Chat/ChatWindow";
-// import DetailsPanel from "../DetailsPanel/DetailsPanel";
+import ChatWindow from "../Chat/ChatWindow";
+import DetailsPanel from "../DetailsPanel/DetailsPanel";
 
 export default function MainLayout() {
   return (
-    <div className="flex h-screen bg-background text-foreground">
-              {/* <SidebarProvider> */}
-      <SidebarWrapper />
-      <div className="flex flex-col flex-1">
-         <Topbar />
-        <div className="flex flex-1 overflow-hidden">
-          <InboxContainer />
-       {/*   <ChatWindow />
-          <DetailsPanel /> */}
-        </div> 
-        this is the main content area
-      </div>
-      {/* </SidebarProvider> */}
+    <div className="flex bg-green-800 h-screen text-foreground overflow-hidden">
+      <SidebarProvider>
+        <SidebarWrapper />
+
+        <div className="flex flex-col w-full h-full bg-red-700">
+          {/* Topbar */}
+          <Topbar />
+
+          <div className="grid min-h-dvh grid-cols-[auto_1fr_auto]">
+            <div className="overflow-auto"><InboxContainer /></div>
+            <div className="overflow-auto"><ChatWindow /></div>
+            <div className="overflow-auto"><DetailsPanel /></div>
+          </div>
+        </div>
+      </SidebarProvider>
     </div>
   );
 }
+
