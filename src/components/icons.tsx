a354edcb22449578a3091242174f8068e3af3fb2
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-01 05:53:45 -0700

    feat: add SvgIcon component with menu, menuMotion, and chat icons

---
+++ src/components/icons.tsx
@@ -0,0 +1,42 @@
+type SvgIconProps = React.SVGProps<SVGSVGElement>;
+
+export const Icons = {
+    menu: (props: SvgIconProps) => (
+        <svg
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            height="1em"
+            width="1em"
+            {...props}
+        >
+            <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z" />
+        </svg>
+    ),
+    menuMotion: (props: SvgIconProps) => (
+        <svg fill="none" viewBox="0 0 24 24" height="1em" width="1em" {...props}>
+            <path
+                fill="currentColor"
+                d="M12 5a1 1 0 100 2h8a1 1 0 100-2h-8zM7 12a1 1 0 011-1h8a1 1 0 110 2H8a1 1 0 01-1-1zM3 18a1 1 0 011-1h8a1 1 0 110 2H4a1 1 0 01-1-1z"
+            />
+        </svg>
+    ),
+    chat: (props: SvgIconProps) => (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-message-square-text-icon lucide-message-square-text"
+            {...props}
+        >
+            <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" />
+            <path d="M13 8H7" />
+            <path d="M17 12H7" />
+        </svg>
+    )
+};
