a54635cdc5f580d4d68b7025824a570a77516e0a
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-01 05:15:14 -0700

    fix: update ChatWindow background color and styling for consistency

--- src/components/Chat/ChatWindow.tsx
+++ src/components/Chat/ChatWindow.tsx
@@ -1,6 +1,6 @@
 export default function ChatWindow() {
     return (
-        <div className="flex h-full bg-amber-400">
+        <div className="flex h-full bg-secondary rounded-2xl p-7">
                 Chat Window
         </div>
     );
