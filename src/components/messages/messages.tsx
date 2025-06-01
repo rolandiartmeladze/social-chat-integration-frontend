9a29a79ecc2c0cfba2d2c3f27f19e9449c9d7d93
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-01 05:15:43 -0700

    fix: remove background color from Messages component for improved styling

--- src/components/messages/messages.tsx
+++ src/components/messages/messages.tsx
@@ -2,7 +2,7 @@
 export default function Messages() {
 
     return (
-        <div className="flex bg-amber-800 p-9 h-full">
+        <div className="flex p-9 h-full">
             messages
         </div>
 
