b8f6f60b9c1261791cd1a5ca7c59f6a56bb4388f
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-01 05:15:27 -0700

    fix: update import for DetailsSection and adjust layout styling

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -3,19 +3,19 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 import Topbar from "@/components/Layout/Topbar";
 import InboxContainer from "../Inbox/InboxContainer";
 import ChatWindow from "../Chat/ChatWindow";
-import DetailsPanel from "../DetailsPanel/DetailsPanel";
+import DetailsSection from "../DetailsPanel/DetailsSection";
 
 export default function MainLayout() {
   return (
     <div className="flex bg-background h-screen text-foreground overflow-hidden">
       <SidebarProvider>
         <SidebarWrapper />
-        <div className="flex flex-col w-full h-full bg-red-700">
+        <div className="flex pb-3 flex-col w-full h-full">
           <Topbar />
-          <div className="grid min-h-dvh grid-cols-[auto_1fr_auto]">
-            <div className="overflow-auto"><InboxContainer /></div>
-            <div className="overflow-auto"><ChatWindow /></div>
-            <div className="overflow-auto"><DetailsPanel /></div>
+          <div className="grid min-h-dvh gap-2 p-3 grid-cols-[auto_1fr_auto]">
+            <InboxContainer />
+            <ChatWindow />
+            <DetailsSection />
           </div>
         </div>
       </SidebarProvider>
