7be0e6d6a92c5bb0b7d6483fd99a204756545141
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-02 09:34:18 -0700

    feat: add panelLeft, arrowDown, and arrowUp icons to SvgIcon component

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -38,5 +38,66 @@ export const Icons = {
             <path d="M13 8H7" />
             <path d="M17 12H7" />
         </svg>
+    ),
+    panelLeft: (props: SvgIconProps) => (
+        <svg xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            {...props}
+            className="lucide lucide-panel-left-icon lucide-panel-left"
+        >
+            <rect
+                width="18"
+                height="18"
+                x="3"
+                y="3"
+                rx="2"
+            />
+            <path d="M9 3v18" />
+        </svg>
+    ),
+    arrowDown: (props: SvgIconProps) => (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            {...props}
+            className="lucide lucide-chevron-down-icon lucide-chevron-down"
+        >
+            <path
+                d="m6 9 6 6 6-6"
+            />
+        </svg>
+    ),
+    arrowUp: (props: SvgIconProps) => (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none" stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            {...props}
+            className="lucide lucide-chevron-up-icon lucide-chevron-up"
+        >
+            <path
+                d="m18 15-6-6-6 6"
+            />
+        </svg>
     )
+
 };
