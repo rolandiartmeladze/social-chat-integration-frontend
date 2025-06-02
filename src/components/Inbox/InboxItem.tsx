809e6f8741e58ef6a13e3387d221e55558d93d62
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-02 10:36:38 -0700

    feat: implement ConversationItem component to display conversation details

--- src/components/Inbox/InboxItem.tsx
+++ src/components/Inbox/InboxItem.tsx
@@ -1,7 +1,27 @@
-export default function InboxItem() {
+import { Conversation } from "@/models/conversation.model";
+import { Avatar, AvatarImage } from "../ui/avatar";
+interface ConversationProps {
+    conv: Conversation;
+}
+
+export default function ConversationItem({ conv }: ConversationProps) {
+
     return (
-        <div className="flex items-center p-4 border-b border-gray-200 hover:bg-gray-50">
-            Inbox Item
-        </div>
+        <li
+            key={conv.id}
+            className="p-3 flex gap-1 border-b-2"
+        >
+            <Avatar className="size-7">
+                <AvatarImage src={conv.participants[0].avatarUrl} alt={conv.participants[0].name} className="w-6 h-6 rounded-full" />
+            </Avatar>
+            <div className="flex flex-col gap-2">
+
+            
+            <div className="text-lg">{conv.participants[0].name}</div>
+            <p className="text-foreground">
+                {conv.messages.length > 0 ? ` ${conv.messages[0].content}` : 'No messages yet'}
+            </p>
+            </div>
+        </li>
     )
 }
