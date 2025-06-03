cd6c82f41df0174098242b978f6b52eb97545948
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-02 17:05:12 -0700

    feat: replace placeholder text with ChatMessages component in ChatWindow

--- src/components/Chat/ChatWindow.tsx
+++ src/components/Chat/ChatWindow.tsx
@@ -1,7 +1,9 @@
+import ChatMessages from "./ChatMessage";
+
 export default function ChatWindow() {
     return (
         <div className="flex h-full bg-secondary rounded-2xl p-7">
-                Chat Window
+                <ChatMessages />
         </div>
     );
 }
