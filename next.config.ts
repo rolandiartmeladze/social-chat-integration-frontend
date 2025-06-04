74ff7b06f0f2278c00cadd17ed395c6f44f605bf
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-04 07:22:53 -0700

    fix: add image domain configuration to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['scontent-iad3-2.xx.fbcdn.net'],
+  },
 };
 
 export default nextConfig;
