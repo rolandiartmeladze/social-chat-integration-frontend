0096b997656fe54d0809c333ecfeea7ac872f0ff
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-04 10:31:53 -0700

    fix: handle optional chaining for messages in ChatMessages component

--- src/components/Chat/ChatMessage.tsx
+++ src/components/Chat/ChatMessage.tsx
@@ -8,13 +8,12 @@ import { UserAvatar } from "../UserAvatar/avatar";
 export default function ChatMessages() {
   const { activeConvData } = useConversation();
   console.log(activeConvData);
-
   if (!activeConvData) return <p>No participant found</p>;
 
   return (
     <ScrollArea className="h-full px-4 py-2">
       <ul className="flex flex-col gap-3">
-        {activeConvData.messages?.map((msg: Message, index: number) => (
+        {activeConvData?.messages?.map((msg: Message, index: number) => (
           <li key={index} className="flex gap-2 items-start bg-accent/30 p-2 rounded">
             <UserAvatar
               src={msg.sender.avatarUrl}
