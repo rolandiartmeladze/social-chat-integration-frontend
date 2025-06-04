0cdba62d22fbfb780d45edee3058a628b63ddb75
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-04 07:22:58 -0700

    feat: add userAvatar, add, and smile icons to Icons component

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -98,6 +98,61 @@ export const Icons = {
                 d="m18 15-6-6-6 6"
             />
         </svg>
-    )
-
+    ),
+    userAvatar: (props: SvgIconProps) => (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-circle-user-round-icon lucide-circle-user-round"
+            {...props}
+        >
+            <path d="M18 20a6 6 0 0 0-12 0" />
+            <circle cx="12" cy="10" r="4" />
+            <circle cx="12" cy="12" r="10" />
+        </svg>
+    ),
+    add: (props: SvgIconProps) => (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-circle-plus-icon lucide-circle-plus"
+            {...props}
+        >
+            <circle cx="12" cy="12" r="10" />
+            <path d="M8 12h8" />
+            <path d="M12 8v8" />
+        </svg>
+    ),
+    smile: (props: SvgIconProps) => (
+        <svg xmlns="http://www.w3.org/2000/svg"
+            width="24" height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round" 
+            className="lucide lucide-smile-icon lucide-smile"
+            {...props}
+            >
+            <circle cx="12" cy="12" r="10" />
+            <path d="M8 14s1.5 2 4 2 4-2 4-2" />
+            <line x1="9" x2="9.01" y1="9" y2="9" />
+            <line x1="15" x2="15.01" y1="9" y2="9" />
+        </svg>
+    ),
 };
