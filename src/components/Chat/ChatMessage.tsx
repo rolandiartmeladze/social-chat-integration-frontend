954af041035391eabc906ad3d6385e172636fa84
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-04 07:23:16 -0700

    fix: update ChatMessages component to improve message rendering and layout

--- src/components/Chat/ChatMessage.tsx
+++ src/components/Chat/ChatMessage.tsx
@@ -3,26 +3,29 @@
 import { useConversation } from "@/context/ConversationContext";
 import { Message } from "@/models/conversation.model";
 import { ScrollArea } from "../ui/scroll-area";
+import { UserAvatar } from "../UserAvatar/avatar";
 
 export default function ChatMessages() {
-    const { activeConvData } = useConversation();
-    console.log(activeConvData);
-    if (!activeConvData) return <p>need selecte conversation</p>;
-    return (
-        <div className="flex flex-col gap-3 w-full  bg-muted/20">
+  const { activeConvData } = useConversation();
+  console.log(activeConvData);
+  //   const participant = activeConvData?.participants?.[0];
 
-            <h1 className="w-full">
-                Chat Messages
-            </h1>
-            <ScrollArea>
-                <ul className="flex flex-col gap-2">
-                    {activeConvData.messages?.map((msg: Message, index: number) => (
-                        <li className="text-sm mt-2 bg-accent border-b" key={index}>
-                            {msg?.text}
-                            </li>
-                    ))}
-                </ul>
-            </ScrollArea>
-        </div>
-    );
+  if (!activeConvData) return <p>No participant found</p>;
+
+  return (
+    <ScrollArea className="h-full px-4 py-2">
+      <ul className="flex flex-col gap-3">
+        {activeConvData.messages?.messages?.map((msg: Message, index: number) => (
+          <li key={index} className="flex gap-2 items-start bg-accent/30 p-2 rounded">
+            <UserAvatar 
+            src={msg.sender.avatarUrl} 
+            alt={msg.sender.name?.slice(0, 2).toUpperCase()} 
+            />
+
+            <p className="text-sm">{msg.text}</p>
+          </li>
+        ))}
+      </ul>
+    </ScrollArea>
+  );
 }
