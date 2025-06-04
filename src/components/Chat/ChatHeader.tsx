0867ded1408200b30ce7f0560adc3fd98303df01
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-04 07:23:11 -0700

    fix: implement ChatHeader component with participant details and styling

--- src/components/Chat/ChatHeader.tsx
+++ src/components/Chat/ChatHeader.tsx
@@ -1,8 +1,20 @@
-export default function ChatHeader() {
-    return (
-        <div className="flex w-full bg-blue-800 h-screen">
-            Chat Header
-        </div>
-    );
-}
+"use client";
 
+import { useConversation } from "@/context/ConversationContext";
+import { UserAvatar } from "../UserAvatar/avatar";
+
+export default function ChatHeader() {
+  const { activeConvData } = useConversation();
+  const participant = activeConvData?.messages?.participants[1];
+  if (!participant) return <p>No participant found</p>;
+
+  return (
+    <div className="flex items-center w-full p-4 h-16 border-b">
+      <UserAvatar 
+      src={participant.avatarUrl} 
+      alt={participant.name?.slice(0, 2).toUpperCase()} 
+      />
+      <h3 className="text-md font-medium">{participant.name}</h3>
+    </div>
+  );
+}
