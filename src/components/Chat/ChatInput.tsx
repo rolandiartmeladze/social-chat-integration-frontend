22887f81020d259db87cb21f18afae0c8c8ea789
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-04 07:23:05 -0700

    fix: update ChatInput component layout and styling for better user experience

--- src/components/Chat/ChatInput.tsx
+++ src/components/Chat/ChatInput.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import { Icons } from "../icons";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+
 export default function ChatInput() {
-    return (
-        <div className="flex w-full bg-blue-800 h-screen">
-            Chat Input
+  return (
+    <div className="flex w-full p-3 gap-2 items-center bg-secondary/60 border-t">
+     <Icons.add />
+      <Input className="border-none outline-none " placeholder="Write your message..." />
+      <div className="flex gap-1 items-center">
+        <Icons.smile /> 
+        <Button className="bg-muted/60 hover:bg-muted cursor-pointer rounded-2xl" variant="secondary" size="sm">send</Button>
         </div>
-    );
+    </div>
+  );
 }
