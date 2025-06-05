57a7e122cf21729fee2938a1c093a316fe679f6a
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-05 08:23:48 -0700

    refactor: remove social media chat components from MessagesPage and update content structure

--- src/app/user/messages/page.tsx
+++ src/app/user/messages/page.tsx
@@ -1,15 +1,10 @@
-import InstagramChat from '@/components/instagramChat/instagram-chat';
-import MessengerChat from '@/components/messangerChat/messenger-chat';
-import TelegramChat from '@/components/telegramChat/telegramChat';
 
 export default function MessagesPage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       MessagesPage
 <div className="flex flex-rows-2 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <MessengerChat />
-      <InstagramChat />
-      <TelegramChat />
+  hear is user messages content
       </div>
     </div>
   );
