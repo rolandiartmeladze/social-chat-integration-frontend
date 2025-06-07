861d6195f53da8a7182ff09c3964204f96cbdf0a
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-07 15:07:37 -0700

    fix: update participant handling in ChatHeader to correctly display user information

--- src/components/Chat/ChatHeader.tsx
+++ src/components/Chat/ChatHeader.tsx
@@ -2,22 +2,24 @@
 
 import { useConversation } from "@/context/ConversationContext";
 import { UserAvatar } from "../UserAvatar/avatar";
-import { User } from "@/models/conversation.model";
 
 export default function ChatHeader() {
   const { activeConvData } = useConversation();
-console.log(activeConvData);
-  const participant:User | undefined = activeConvData?.participants[1];
+  console.log(activeConvData);
 
-  if (!participant) return <p>No participant found</p>;
+  if (!activeConvData?.participants) return <p>No participant found</p>;
 
   return (
     <div className="flex items-center w-full p-4 h-16 border-b">
-      <UserAvatar 
-      src={participant.avatarUrl} 
-      alt={participant.name?.slice(0, 2).toUpperCase()} 
-      />
-      <h3 className="text-md font-medium">{participant.name}</h3>
+      {activeConvData.participants.user &&
+        <>
+          <UserAvatar
+            src={activeConvData?.participants.user.avatarUrl}
+            alt={activeConvData?.participants.user.name?.slice(0, 2).toUpperCase()}
+          />
+          <h3 className="text-md font-medium">{activeConvData?.participants.user.name}</h3>
+        </>
+        }
     </div>
   );
 }
