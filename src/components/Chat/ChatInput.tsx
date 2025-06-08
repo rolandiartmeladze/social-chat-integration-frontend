8b67ca60c8bb6336bffa09540c64cffcccf7eab9
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-08 08:20:38 -0700

    fix: implement message sending functionality in ChatInput component

--- src/components/Chat/ChatInput.tsx
+++ src/components/Chat/ChatInput.tsx
@@ -1,18 +1,60 @@
 "use client";
 
+import axios from "axios";
+import { useState } from "react";
 import { Icons } from "../icons";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
+import { useConversation } from "@/context/ConversationContext";
 
 export default function ChatInput() {
+  const { activeConvData } = useConversation();
+  const [textMessage, setTextMessage] = useState("");
+
+  if (!activeConvData) return <p>Need to select a conversation</p>;
+
+  const recipientId = activeConvData.participants.user.id;
+
+  const sendMessage = async () => {
+    if (!textMessage.trim()) return;
+
+    try {
+      await axios.post(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/messenger/send`,
+        {
+          sender: recipientId,
+          text: textMessage,
+        }
+      );
+
+      setTextMessage("");
+    } catch (error) {
+      console.error("Backend /messenger/send error:", error);
+      alert("შეტყობინების გაგზავნა ვერ მოხერხდა.");
+    }
+  };
+
   return (
     <div className="flex w-full py-1 px-3 gap-2 items-center bg-secondary/60 border-t">
-     <Icons.add className="size-9" />
-      <Input className="border-none outline-none " placeholder="Write your message..." />
+      <Icons.add className="size-9" />
+      <Input
+        className="border-none outline-none"
+        placeholder="Write your message..."
+        value={textMessage}
+        onChange={(e) => setTextMessage(e.target.value)}
+        onKeyDown={(e) => e.key === "Enter" && sendMessage()}
+      />
       <div className="flex gap-1 items-center">
-        <Icons.smile /> 
-        <Button className="bg-muted/60 hover:bg-muted cursor-pointer rounded-2xl" variant="secondary" size="sm">send</Button>
-        </div>
+        <Icons.smile />
+        <Button
+          className="bg-muted/60 hover:bg-muted cursor-pointer rounded-2xl"
+          variant="secondary"
+          size="sm"
+          onClick={sendMessage}
+        >
+          Send
+        </Button>
+      </div>
     </div>
   );
 }
