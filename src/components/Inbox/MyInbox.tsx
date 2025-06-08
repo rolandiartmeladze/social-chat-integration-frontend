e2cb82a83c1fe34f1252938ec761167985d25bb0
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-08 12:21:49 -0700

    feat: add MyInboxCommand component for inbox command functionality

---
+++ src/components/Inbox/MyInbox.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import {
+    Command,
+    CommandGroup,
+    CommandItem,
+    CommandList,
+    CommandSeparator,
+} from "@/components/ui/command"
+import { UserAvatar } from "../UserAvatar/avatar"
+import { Icons } from "@/components/icons"
+
+export default function MyInboxCommand() {
+
+    return (
+        <div>
+            <Command className="h-full">
+                <CommandList>
+                    <CommandGroup>
+                        {[
+                            {
+                                icon: <UserAvatar
+                                    className="size-5"
+                                    src="https://avatars.githubusercontent.com/u/13499054?v=4"
+                                    alt="My_Inbox"
+                                />,
+                                label: "My Inbox",
+                                count: 4,
+                                customIcon: true,
+                            },
+                            { icon: <Icons.users />, label: "All", count: 5 },
+                            { icon: <Icons.clock />, label: "Unassigned", count: 4 },
+                            { icon: <Icons.mention />, label: "Mentions", count: 0 },
+                            { icon: <Icons.pencil />, label: "My Outbox", count: 0 },
+                            { icon: <Icons.check />, label: "All Resolved", count: 4 },
+                        ].map(({ icon, label, count, customIcon }, i) => (
+                            <CommandItem
+                                key={i}
+                                className="flex justify-between items-center"
+                            >
+                                <div className="flex items-center gap-2 cursor-pointer">
+                                    {customIcon ? icon : icon}
+                                    <samp>{label}</samp>
+                                </div>
+                                <samp>{count}</samp>
+                            </CommandItem>
+                        ))}
+                    </CommandGroup>
+                    <CommandSeparator />
+                </CommandList>
+            </Command>
+        </div>
+    )
+}
