605a7976523f5f34d25878386e2a90415a5a30b7
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-08 12:21:44 -0700

    feat: implement InBoxSidebar component with InboxHeader and icon buttons

---
+++ src/components/Inbox/InBoxSidebar.tsx
@@ -0,0 +1,20 @@
+import InboxHeader from "./InboxHeader";
+import { Icons } from "@/components/icons";
+import MyInboxCommand from "./MyInbox";
+
+export default function InBoxSidebar() {
+
+    return (
+        <div className="w-72 mx-auto h-full">
+            <InboxHeader>
+                <h2 className="text-xl">Messages</h2>
+                <div className="flex gap-2">
+                    <samp className="bg-[#515151] flex items-center justify-center size-9 rounded-full p-2"><Icons.search className="size-4" /></samp>
+                    <samp className="bg-[#515151] flex items-center justify-center size-9 rounded-full p-2"><Icons.add className="size-4" /></samp>
+                </div>
+
+            </InboxHeader>
+            <MyInboxCommand />
+        </div>
+    )
+}
