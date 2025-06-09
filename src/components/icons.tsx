1c5528348805faf4246b88032c9300c991cda3a9
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-09 05:28:20 -0700

    feat: add new SVG icons for mail, comments, and ticket

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -383,5 +383,63 @@ export const Icons = {
             <path d="M21.801 10A10 10 0 1 1 17 3.335" />
             <path d="m9 11 3 3L22 4" />
         </svg>
+    ),
+    mail: (props: SvgIconProps) => (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            {...props}
+            className="lucide lucide-mail-icon lucide-mail"
+        >
+            <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />
+            <rect x="2" y="4" width="20" height="16" rx="2" />
+        </svg>
+    ),
+    comments: (props: SvgIconProps) => (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            {...props}
+            className="lucide lucide-message-square-more-icon lucide-message-square-more"
+        >
+            <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" />
+            <path d="M8 10h.01" />
+            <path d="M12 10h.01" />
+            <path d="M16 10h.01" />
+        </svg>
+    ),
+    ticket: (props: SvgIconProps) => (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            {...props}
+            className="lucide lucide-ticket-icon lucide-ticket"
+        >
+            <path d="M2 9a3 3 0 0 1 0 6v2a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-2a3 3 0 0 1 0-6V7a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2Z" />
+            <path d="M13 5v2" />
+            <path d="M13 17v2" />
+            <path d="M13 11v2" />
+        </svg>
     )
 };
