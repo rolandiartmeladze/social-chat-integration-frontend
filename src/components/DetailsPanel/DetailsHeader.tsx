86eaad600d1186cfc74dced64a24a9650c3d4d69
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-09 16:29:39 -0700

    feat: add DetailsHeader component with icons for improved UI

---
+++ src/components/DetailsPanel/DetailsHeader.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { Icons } from "../icons";
+
+export default function DetailsHeader() {
+    return (
+        <div className="flex items-center justify-between w-full p-4 h-16 border-b gap-11">
+            <div className="flex gap-7">
+                <samp>Details</samp>
+                <samp className="flex gap-3"><Icons.stars className="size-6" />  <span>Copilote</span></samp>
+            </div>
+            <samp><Icons.panelRight className="size-4" /></samp>
+        </div>
+    );
+}
