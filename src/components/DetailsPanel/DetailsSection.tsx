d454872114dfc457e35ba8e47058d2b4fe6f485b
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-09 16:29:49 -0700

    refactor: update DetailsSection layout to use Accordion for better organization

--- src/components/DetailsPanel/DetailsSection.tsx
+++ src/components/DetailsPanel/DetailsSection.tsx
@@ -1,7 +1,54 @@
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion";
+
 export default function DetailsSection() {
     return (
-        <div className="flex w-full p-7 h-full bg-secondary mr-2 rounded-2xl">
-            <h2 className="text-lg font-semibold mb-4">Details Section</h2>
+        <div className="p-2">
+            <Accordion type="multiple"
+                defaultValue={["general", "contact-info", "device-info", "activity-logs"]} 
+                >
+                <AccordionItem value="general">
+                    <AccordionTrigger className="text-lg px-2 py-1 hover:bg-secondary rounded-md">
+                        General
+                    </AccordionTrigger>
+                    <AccordionContent>
+                        General Details content
+                    </AccordionContent>
+                </AccordionItem>
+
+                <AccordionItem value="contact-info">
+                    <AccordionTrigger className="text-lg px-2 py-1 hover:bg-secondary rounded-md">
+                        Contact Info
+                    </AccordionTrigger>
+                    <AccordionContent>
+                        Contacts Details
+                    </AccordionContent>
+                </AccordionItem>
+
+                <AccordionItem value="device-info">
+                    <AccordionTrigger className="text-lg px-2 py-1 hover:bg-secondary rounded-md">
+                        Device Info
+                    </AccordionTrigger>
+                    <AccordionContent>
+                        Device Inf Details
+                    </AccordionContent>
+                </AccordionItem>
+
+                <AccordionItem value="activity-logs">
+                    <AccordionTrigger className="text-lg px-2 py-1 hover:bg-secondary rounded-md">
+                        Activity Logs
+                    </AccordionTrigger>
+                    <AccordionContent>
+                        Activity Logs Details
+                    </AccordionContent>
+                </AccordionItem>
+
+            </Accordion>
+
         </div>
     );
 }
