11f236a7db87e5d61527bdc473cb4290902c0702
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-10 04:51:18 -0700

    feat: add DetailsConfig for structured display of user details

---
+++ src/types/Details.tsx
@@ -0,0 +1,23 @@
+export const DetailsConfig = {
+  General: [
+    { label: "Status", icon: "progress", result: "In Progress" },
+    { label: "Ticket/Conversation ID", icon: "ticket", result: "TG1789035" },
+    { label: "Assignee", icon: "userAvatar", result: "Giorgi Vashakidze" },
+    { label: "Team Inbox", icon: "unassigned", result: "Unassigned" },
+  ],
+  Contact: [
+    { label: "Name", icon: "userAvatar", result: "Alex Chachava" },
+    { label: "E-Mail", icon: "mail", result: "user@example.com" },
+    { label: "Phone", icon: "phone", result: "+995 599 123 456" },
+    { label: "Social Handle", icon: "mention", result: "eidtsumnemlla" },
+  ],
+  Device: [
+    { label: "IP Address", icon: "ip", result: "176.221.162.49" },
+    { label: "Country", icon: "flag", result: "Georgia, Tbilisi" },
+    { label: "Device", icon: "android", result: "Android" },
+  ],
+  Activity: [
+    { label: "Last Login", icon: "clock", result: "Today, 11:00 AM" },
+    { label: "Status", icon: "active", result: "Active" },
+  ],
+};
