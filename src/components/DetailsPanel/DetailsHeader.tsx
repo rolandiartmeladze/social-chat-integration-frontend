14f50d4a84b8f44417f23b9d606834dcd8a2b25c
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-10 04:50:57 -0700

    fix: update DetailsHeader styling for improved text visibility

--- src/components/DetailsPanel/DetailsHeader.tsx
+++ src/components/DetailsPanel/DetailsHeader.tsx
@@ -5,7 +5,7 @@ export default function DetailsHeader() {
     return (
         <div className="flex items-center justify-between w-full p-4 h-16 border-b gap-11">
             <div className="flex gap-7">
-                <samp>Details</samp>
+                <samp className="text-lg font-bold underline">Details</samp>
                 <samp className="flex gap-3"><Icons.stars className="size-6" />  <span>Copilote</span></samp>
             </div>
             <samp><Icons.panelRight className="size-4" /></samp>
