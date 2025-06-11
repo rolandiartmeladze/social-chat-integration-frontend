0118612c2205ff758937d2b24aa0566abd1b7743
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-11 08:15:19 -0700

    feat: create AuthLayout component for authentication layout structure

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,11 @@
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="grid min-h-dvh grid-rows-[auto_1fr]">
+      {children}
+    </div>
+  );
+}
