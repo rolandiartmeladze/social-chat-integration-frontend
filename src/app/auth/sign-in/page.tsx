aaa4684ed66af720a1f4efc7646c1c82e52314d5
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-11 09:08:29 -0700

    feat: add logout functionality to SignIn component

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,14 +1,23 @@
 "use client";
 import GoogleButton from 'react-google-button';
+import { Button } from '@/components/ui/button';
+
 export default function SignIn() {
     const BackendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
     const handleGoogleLogin = () => {
     window.location.href = `${BackendUrl}/auth/google`;
   };
+      const handleLogOut = () => {
+    window.location.href = `${BackendUrl}/auth/logout`;
+  };
+
 
   return (
         <div className="flex flex-col gap-6 h-screen justify-center items-center">
           <GoogleButton onClick={handleGoogleLogin} />
+          <Button onClick={handleLogOut} >
+            Log Out
+          </Button>
         </div>
   );
 }
