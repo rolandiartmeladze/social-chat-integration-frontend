faac9bb67f6c3f93abc990b64727a0ca88905935
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-12 03:04:04 -0700

    refactor: simplify SignIn component by removing unused useEffect and streamlining sign-in logic

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,37 +1,10 @@
 "use client";
-
-import { useEffect } from "react";
-import GoogleButton from "react-google-button";
-import { Button } from "@/components/ui/button";
+import GoogleButton from 'react-google-button';
+import { Button } from '@/components/ui/button';
 
 export default function SignIn() {
-  useEffect(() => {
-    const handleMessage = (event: MessageEvent) => {
-      if (event.origin !== process.env.NEXT_PUBLIC_FRONTEND_URL) return;
-
-      if (event.data === "authenticated") {
-        console.log("✅ Authenticated!");
-        window.location.href = "/user/messages";
-      }
-    };
-
-    window.addEventListener("message", handleMessage);
-
-    return () => {
-      window.removeEventListener("message", handleMessage);
-    };
-  }, []);
-
   const handleSignIn = () => {
-    const authWindow = window.open(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/google`,
-      "googleOAuthPopup",
-      "width=500,height=600"
-    );
-
-    if (!authWindow) {
-      alert("გთხოვთ, დაუშვათ popup ფანჯრები ბრაუზერში.");
-    }
+    window.location.href = `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/google`;
   };
 
   const handleLogOut = () => {
@@ -41,9 +14,7 @@ export default function SignIn() {
   return (
     <div className="flex flex-col gap-6 h-screen justify-center items-center">
       <GoogleButton onClick={handleSignIn} />
-      <Button onClick={handleLogOut}>
-        Log Out
-      </Button>
+      <Button onClick={handleLogOut}>Log Out</Button>
     </div>
   );
 }
