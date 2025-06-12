8c789db6d359a9d308a40a475b585322e1881378
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-12 00:56:03 -0700

    refactor: destructure user from useAuth in AuthCallback component

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import { useAuth } from '@/hooks/useAuth';
+
 export default function AuthCallback() {
-const auth = useAuth();
-if (!auth.user) return <p>Loading or not authenticated...</p>;
- 
-return (
-    <div>
-      <h2>Welcome, {auth.user.displayName}!</h2>
-      {auth.user.photos && <img src={auth.user.photos[0]?.value} alt="avatar" />}
-    </div>
-  );
+    const { user } = useAuth();
+
+    if (!user) return <p>Loading or not authenticated...</p>;
+
+    return (
+        <div>
+            <h2>Welcome, {user.displayName}!</h2>
+            {user.photos && <img src={user.photos[0]?.value} alt="avatar" />}
+        </div>
+    );
 }
