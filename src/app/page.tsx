d17adc97ac5cf01e517a48ea08be441c0273eb32
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-12 02:54:41 -0700

    refactor: enhance layout and user flow in Home component with conditional rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,35 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-
+import { useAuth } from "@/hooks/useAuth";
 export default function Home() {
   const SignIn = () => {
     window.location.href = `/auth/sign-in`;
   };
 
+  const Messages = () => {
+    window.location.href = `/auth/sign-in`;
+  };
+
+  const {user} = useAuth();
+
   return (
-    <main className="flex flex-col">
-      <h1>
-        <Button onClick={SignIn}>
+    <main className="flex flex-col h-screen justify-center items-center">
+      {!user ? 
+        <Button variant="secondary" onClick={SignIn}>
           Go Auth Page
         </Button>
-      </h1>
+        :  
+        <>
+        <h1>
+          Welcome {user.displayName}
+        </h1>
+        <Button variant="outline" onClick={Messages}>
+          Go Messages page
+        </Button>       
+        </>     
+
+        }
     </main>
   );
 }
