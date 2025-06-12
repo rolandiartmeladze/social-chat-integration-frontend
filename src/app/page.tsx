700ad67116cde98385daad8b2b744c89f0c5a098
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-12 01:26:57 -0700

    refactor: format code and ensure "use client" directive is present in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const SignIn = () => {
-    window.location.href = `/auth/sign-in`;}
+    window.location.href = `/auth/sign-in`;
+  };
+
   return (
-      <main className="flex flex-col">
-        <h1>
-          <Button onClick={SignIn}>
-            Go Auth Page
-          </Button>
-        </h1>
-      </main>
+    <main className="flex flex-col">
+      <h1>
+        <Button onClick={SignIn}>
+          Go Auth Page
+        </Button>
+      </h1>
+    </main>
   );
 }
