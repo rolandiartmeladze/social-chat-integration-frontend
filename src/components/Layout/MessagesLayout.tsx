076b94ddffb6a75085d4c98807a3bdc932af8b60
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-13 05:20:46 -0700

    feat: create MessagesLayout component for chat interface

---
+++ src/components/Layout/MessagesLayout.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import InboxContainer from "../Inbox/InboxContainer";
+import ChatWindow from "../Chat/ChatWindow";
+import DetailsPanel from "../DetailsPanel/DetailsPanel";
+
+export default function MessagesLayout() {
+  return (
+    <div className="grid gap-2 p-2 grid-cols-[auto_1fr_auto] h-full overflow-auto">
+      <InboxContainer />
+      <ChatWindow />
+      <DetailsPanel />
+    </div>
+  );
+}
+
