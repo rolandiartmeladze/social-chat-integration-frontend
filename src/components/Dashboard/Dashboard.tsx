a34e8d1ccbc5b4fa0a55ee8ae728220cdf54896a
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-13 05:20:33 -0700

    fix: simplify Dashboard component by removing loading state handling and unnecessary user checks

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -3,39 +3,18 @@
 import { useAuth } from "@/app/auth/useAuth/useAuth";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
 export default function Dashboard() {
-  const { user, loading } = useAuth();
+  const { user, isAuthenticated } = useAuth();
   const router = useRouter();
-
-  useEffect(() => {
-    if (!loading && user) {
-      console.log("ავტორიზებული:", user);
-    }
-  }, [user, loading]);
-
-  if (loading) {
-    return (
-      <div className="min-h-screen flex justify-center items-center bg-background text-muted-foreground text-lg">
-        იტვირთება...
-      </div>
-    );
-  }
-
-  if (!user) {
-    return (
-      <div className="min-h-screen flex justify-center items-center bg-background text-muted-foreground text-lg">
-        მომხმარებელი არ არის ავტორიზებული.
-      </div>
-    );
-  }
+  if (isAuthenticated) router.push("/");
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-background px-4">
       <div className="max-w-xl text-center space-y-6 p-8 rounded-2xl shadow-xl border border-border bg-card">
         <h1 className="text-4xl font-bold text-foreground">
-          მოგესალმები, {user?.displayName || user?._json?.name || "მომხმარებელო"} 👋        </h1>
+          მოგესალმები, {user?.displayName || "მომხმარებელო"} 👋
+        </h1>
 
         <p className="text-lg text-muted-foreground leading-relaxed">
           აქ შეგიძლიათ გადახვიდეთ თქვენს მესიჯებზე და მართოთ თქვენი კომუნიკაცია ერთ სივრცეში.
