ccaa2413755d2c1ec31802be7607734ee17b137d
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-13 13:11:42 -0700

    feat: add loading skeletons for conversations while fetching data

--- src/components/Inbox/InboxList.tsx
+++ src/components/Inbox/InboxList.tsx
@@ -6,6 +6,7 @@ import SortConversations from './InboxSort';
 import ConversationItem from './InboxItem';
 import { useConversation } from "@/context/ConversationContext";
 import InboxHeader from './InboxHeader';
+import { Skeleton } from "@/components/ui/skeleton"
 
 export default function InboxList() {
   const {
@@ -13,6 +14,7 @@ export default function InboxList() {
     setActiveConvId,
     activeConvId,
     refreshConversations,
+    isFetchingAllConversations,
   } = useConversation();
 
   useEffect(() => {
@@ -27,22 +29,31 @@ export default function InboxList() {
 
   return (
     <div className="w-85 border-l">
-      <InboxHeader className='border-b justify-start'>
+      <InboxHeader className="border-b justify-start">
         <Icons.panelLeft />
         <samp className="text-xl font-lg">My Inbox</samp>
       </InboxHeader>
       <SortConversations />
+
       <ul className="scroll px-3 flex flex-col gap-3 h-[calc(100vh-220px)] overflow-y-auto">
-        {allConversations.map((conv) => (
-          <ConversationItem
-            isActive={conv.conversationId === activeConvId}
-            key={conv.conversationId}
-            conv={conv}
-            onClick={() => {
-              setActiveConvId(conv.conversationId);
-            }}
-          />
-        ))}
+        {isFetchingAllConversations
+          ? [...Array(6)].map((_, idx) => (
+            <li key={idx} className="flex gap-3 items-center">
+              <Skeleton className="size-10 rounded-full" />
+              <div className="flex flex-col gap-2 w-full">
+                <Skeleton className="h-4 w-3/4" />
+                <Skeleton className="h-3 w-1/2" />
+              </div>
+            </li>
+          ))
+          : allConversations.map((conv) => (
+            <ConversationItem
+              isActive={conv.conversationId === activeConvId}
+              key={conv.conversationId}
+              conv={conv}
+              onClick={() => setActiveConvId(conv.conversationId)}
+            />
+          ))}
       </ul>
     </div>
   );
