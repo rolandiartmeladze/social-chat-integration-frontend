2b056cee51b732769fde59018eb1385a7b642d51
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-13 05:28:43 -0700

    feat: implement authentication redirection in IndexPage component

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,5 +1,16 @@
-import AuthSAuthShell from "@/app//auth/sign-in/page"
+import AuthSAuthShell from "@/app//auth/sign-in/page";
+import { useAuth } from "../auth/useAuth/useAuth";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function IndexPage() {
+  const { isAuthenticated } = useAuth();
+  const router = useRouter();
+  useEffect(() => {
+    if (isAuthenticated) 
+      { router.push("/dashboard") } 
+    else { router.push("/") }
+  }, [isAuthenticated, router])
   return (
     <div>
       <AuthSAuthShell />
