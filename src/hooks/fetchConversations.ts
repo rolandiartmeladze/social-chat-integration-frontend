1dff70e0f5b77b2200a169144bb865acf88e006b
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-16 02:40:23 -0700

    fix: streamline FetchConversations function and improve error handling

--- src/hooks/fetchConversations.ts
+++ src/hooks/fetchConversations.ts
@@ -2,18 +2,13 @@ import axios from "axios";
 import { Conv } from "@/models/conversation.model";
 
 export async function FetchConversations(): Promise<Conv[] | null> {
-    try {
+  try {
     const Backend_Url = process.env.NEXT_PUBLIC_BACKEND_URL;
-    const response = await axios.get(`${Backend_Url}/conversations`);
-
-    const rawConvs = response.data.conversations;
-      console.log(rawConvs);
-    
-    return rawConvs;
-    } catch (error) {
-        console.error("Error fetching conversations:", error);
-        return null;
-    } finally {
-        console.log("request end");
-    }
+    const response = await axios.get<Conv[]>(`${Backend_Url}/conversations`);
+    console.log("Fetched conversations:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching conversations:", error);
+    return null;
+  }
 }
