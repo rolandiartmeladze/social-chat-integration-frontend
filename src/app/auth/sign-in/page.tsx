e279b6e1323a6b6944fd290b5450c3a0ff781094
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-21 06:00:33 -0700

    ref/update facebook button with auth

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -8,19 +8,26 @@ export default function AuthShell() {
   };
 
   return (
-      <div className="bg-secondary-foreground text-foreground rounded-2xl shadow-xl p-10 max-w-md w-full flex flex-col items-center text-center space-y-6">
-        <Image
-          src={logo}
-          alt="Logo"
-          width={60}
-          height={60}
-          className="rounded-full"
-        />
-        <h1 className="text-2xl font-semibold text-foreground">მოგესალმებით!</h1>
-        <p className="text-foreground text-base">
-          სისტემაში შესასვლელად გამოიყენეთ თქვენი Google ანგარიში.
-        </p>
-        <GoogleButton onClick={handleSignIn} label="შესვლა Google-ით" />
-      </div>
+    <div className="bg-secondary-foreground text-foreground rounded-2xl shadow-xl p-10 max-w-md w-full flex flex-col items-center text-center space-y-6">
+      <Image
+        src={logo}
+        alt="Logo"
+        width={60}
+        height={60}
+        className="rounded-full"
+      />
+      <h1 className="text-2xl font-semibold text-foreground">მოგესალმებით!</h1>
+      <p className="text-foreground text-base">
+        სისტემაში შესასვლელად გამოიყენეთ თქვენი Google ანგარიში.
+      </p>
+      <GoogleButton onClick={handleSignIn} label="შესვლა Google-ით" />
+      <button
+        onClick={() => {
+          window.location.href = `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/facebook`;
+        }}
+      >
+        შესვლა Facebook-ით
+      </button>
+    </div>
   );
 }
