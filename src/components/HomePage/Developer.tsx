dc6b41d4c6ab7dbb3cbe0b0cb79d9435a302fdda
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-26 11:59:32 -0700

    ref:/ ui style detail

--- src/components/HomePage/Developer.tsx
+++ src/components/HomePage/Developer.tsx
@@ -5,11 +5,11 @@ import { UserAvatar } from "../UserAvatar/avatar";
 
 export default function Developer() {
   return (
-    <div className="hidden lg:flex items-center gap-4 p-1 m-2 rounded-md hover:bg-muted transition-colors">
+    <div className="flex items-center gap-1 m-1 md:gap-4 p-1 md:m-2 rounded-md hover:bg-muted transition-colors">
       <UserAvatar
         src="https://avatars.githubusercontent.com/u/13499054?v=4"
         alt="Roland Artmeladze Avatar"
-        className="size-9 lg:size-12 border-2"
+        className="size-8 lg:size-12 border-2"
       />
       <div>
         <Link
@@ -18,9 +18,9 @@ export default function Developer() {
           rel="noopener noreferrer"
           className="text-foreground hover:underline focus:outline-none focus:ring-2 focus:ring-ring rounded"
         >
-          <h2 className="text-lg font-semibold">Roland Artmeladze</h2>
+          <h2 className="text-sm md:text-lg font-semibold">Roland Artmeladze</h2>
         </Link>
-        <p className="text-sm text-muted-foreground">Full-Stack Developer</p>
+        <p className="text-xs md:text-md text-muted-foreground">Full-Stack Developer</p>
       </div>
     </div>
   );
