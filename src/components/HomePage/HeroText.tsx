f9a81801da4989e2f58cfeab56645ac4baf0e9e1
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-26 03:17:13 -0700

    ref: /I updated the component according to the changes.

--- src/components/HomePage/HeroText.tsx
+++ src/components/HomePage/HeroText.tsx
@@ -1,14 +1,22 @@
 "use client";
 
 import { useIsMobile } from "@/hooks/use-mobile";
-
+import TypingText from "@/components/ui/typing-text";
 export default function HeroText() {
     const isMobile = useIsMobile();
     return (
-        <div className="flex flex-col w-[55%] lg:w-3/5 items-center h-full justify-center text-center z-20 p-4 md:p-8">
-            <h1 className="text-xl md:text-4xl font-bold leading-tight text-foreground">
-                {!isMobile ? "All Your Chats. One Powerful Space." : "One Hub. All Your Chats."}
-            </h1>
+        <div className="flex gap-4 flex-col w-[55%] lg:w-3/5 items-center h-full justify-center text-center z-20 p-4 md:p-8">
+            <TypingText
+                className="text-xl md:text-4xl h-10 lg:h-20 px-6 py-2 font-bold leading-tight text-foreground"
+                text={[
+                    "All chats, one space.",
+                    "Chat smarter, not harder.",
+                    "Real-time. Cross-platform. Yours.",
+                ]}
+                speed={50}
+                pause={2500}
+                loop={true}
+            />
             <p className="p-2 text-base md:text-xl text-foreground">
                 {!isMobile
                     ? "Tired of switching between apps to stay in touch? Our multi-platform chat hub brings your Messenger, Telegram, and more together — so you can follow conversations, reply in real time, and stay in control."
