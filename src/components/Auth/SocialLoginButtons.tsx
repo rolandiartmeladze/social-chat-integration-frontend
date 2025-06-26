5babcc05a8a72a3b3ad7c767bc7ea4e263b843b4
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-26 11:38:23 -0700

    feat:/ I have extracted the social authentication buttons into a separate component. It takes the providers' data from the corresponding array and returns a set of corresponding buttons.

---
+++ src/components/Auth/SocialLoginButtons.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { providers } from "@/config/loginProvidrs";
+
+
+interface Props {
+    onClick: (provider: string) => void;
+}
+
+export default function SocialLoginButtons({ onClick }: Props) {
+    return (
+        <div className="flex gap-4 md:gap-2 flex-wrap justify-center items-start">
+            {providers.map(({ name, icon, bgColor, textColor = "text-white", hoverColor }) => (
+                <Button
+                    key={name}
+                    onClick={() => onClick(name.toLowerCase())}
+                    variant="outline"
+                    size="lg"
+                    className={`w-12 px-2 cursor-pointer flex items-center justify-center md:justify-start gap-2 ${bgColor} ${textColor} ${hoverColor ?? ""}`}
+                >
+                    <Image src={icon} alt={name} className="size-8" />
+                </Button>
+            ))}
+        </div>
+    );
+}
