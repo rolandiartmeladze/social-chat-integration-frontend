9b4a646b7fd85a04975fed97dff842043ef9aa84
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-26 01:33:55 -0700

    feat/ I exported it as a separate custom element for easy handling and use. I added custom design elements for responsive behavior.

--- src/components/HomePage/HeroSection.tsx
+++ src/components/HomePage/HeroSection.tsx
@@ -2,22 +2,13 @@
 
 import SocialIcons from './SocialIcons';
 import PersonCarousel from './PersonCarousel';
+import HeroText from './HeroText';
 
 export default function HeroSection() {
   return (
-    <div className="flex flex-col md:flex-cols w-full h-full items-end justify-between relative bg-home overflow-hidden">
-      
+    <div className="flex flex-col items-end justify-between w-full h-full bg-home relative overflow-hidden">
       <PersonCarousel />
-
-      <div className="flex flex-col w-3/5 items-center h-full justify-center text-center space-y-6 z-20 p-4 md:p-8">
-        <h1 className="text-4xl md:text-5xl font-bold leading-tight text-foreground">
-          All Your Chats. One Powerful Space.
-        </h1>
-        <p className="text-lg md:text-xl text-foreground">
-          Tired of switching between apps to stay in touch? <br />
-          Our multi-platform chat hub brings your Messenger, Telegram, and more together — so you can follow conversations, reply in real time, and stay in control.
-        </p>
-      </div>
+      <HeroText />
       <SocialIcons />
     </div>
   );
---
+++ src/components/HomePage/HeroText.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useIsMobile } from "@/hooks/use-mobile";
+
+export default function HeroText() {
+    const isMobile = useIsMobile();
+    return (
+        <div className="flex flex-col w-[55%] lg:w-3/5 items-center h-full justify-center text-center z-20 p-4 md:p-8">
+            <h1 className="text-xl md:text-5xl font-bold leading-tight text-foreground">
+                {!isMobile ? "All Your Chats. One Powerful Space." : "One Hub. All Your Chats."}
+            </h1>
+            <p className="p-2 text-base md:text-xl text-foreground">
+                {!isMobile
+                    ? "Tired of switching between apps to stay in touch? Our multi-platform chat hub brings your Messenger, Telegram, and more together — so you can follow conversations, reply in real time, and stay in control."
+                    : "Bring Messenger, Telegram & more together — reply fast, stay in control."}
+            </p>
+        </div>
+    );
+}
