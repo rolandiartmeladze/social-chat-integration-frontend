f6a3861dd3cf904ae049d5f1e4947f6bfb5698c7
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-26 11:48:54 -0700

    ref: / update small changes

--- src/config/loginProvidrs.ts
+++ src/config/loginProvidrs.ts
@@ -3,24 +3,25 @@ import facebookIcon from "@/../public/Facebook_icon.png";
 import telegramIcon from "@/../public/Telegram_icon.png";
 
 export const providers = [
-  {
-    name: "Google",
-    icon: googleIcon,
-    bgColor: "bg-foreground",
-    textColor: "text-background",
-    hoverColor: "hover:bg-secondary"
-  },
-  {
-    name: "Facebook",
-    icon: facebookIcon,
-    bgColor: "bg-[#3b5998]",
-    textColor: "text-white"
-  },
-  {
-    name: "Telegram",
-    icon: telegramIcon,
-    bgColor: "bg-[#0088cc]",
-    textColor: "text-white",
-    hoverColor: "hover:bg-[#007bb5]"
-  }
+    {
+        name: "Google",
+        icon: googleIcon,
+        bgColor: "bg-[#ffffff]",
+        textColor: "text-background",
+        hoverColor: "hover:bg-secondary"
+    },
+    {
+        name: "Facebook",
+        icon: facebookIcon,
+        bgColor: "bg-[#3b5998]",
+        textColor: "text-foreground",
+        hoverColor: "hover:bg-secondary"
+    },
+    {
+        name: "Telegram",
+        icon: telegramIcon,
+        bgColor: "bg-[#0088cc]",
+        textColor: "text-foreground",
+        hoverColor: "hover:bg-secondary"
+    }
 ];
