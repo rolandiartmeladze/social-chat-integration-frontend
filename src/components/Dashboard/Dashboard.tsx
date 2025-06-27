01b57275b989a3721c54a2180b726378174aac69
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-27 13:21:27 -0700

    feat:/ initial commit with User dashboard component UI

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,37 +1,9 @@
 "use client";
 
-import { useAuth } from "@/app/auth/useAuth/useAuth";
-import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
-
 export default function Dashboard() {
-  const { user, isAuthenticated } = useAuth();
-  const router = useRouter();
-  if (!isAuthenticated) router.push("/");
-
   return (
-    <div className="max-w-xl text-center space-y-6 p-8 rounded-2xl shadow-xl border border-border bg-secondary-foreground">
-      <h1 className="text-4xl font-bold text-foreground">
-        მოგესალმები, {user?.name || "მომხმარებელო"} 👋
-      </h1>
-
-      <p className="text-lg text-muted-foreground leading-relaxed">
-        აქ შეგიძლიათ გადახვიდეთ თქვენს მესიჯებზე და მართოთ თქვენი კომუნიკაცია ერთ სივრცეში.
-      </p>
-
-      <div className="flex flex-col sm:flex-row justify-center gap-4 pt-4">
-        <Button onClick={() => router.push("/user/messages")} size="lg">
-          📩 გადადი მესიჯებზე
-        </Button>
-        <Button onClick={() =>
-          router.push(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/logout`)}
-          size="lg"
-          variant="outline"
-          className="border-muted-foreground text-muted-foreground hover:text-foreground"
-        >
-          🚪 გამოსვლა
-        </Button>
-      </div>
+    <div className="flex flex-col items-center justify-center h-full">
+      <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
     </div>
   );
 }
