fbba98aee7a89c88bed4544dee7837e2de5891b1
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-27 16:59:41 -0700

    ref:/  update dashboard basic structure with beter user experance

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import UserProfile from "./UserProfile/UserProfile";
+
 export default function Dashboard() {
   return (
     <div className="grid grid-cols-[auto_1fr] grid-rows-[auto_1fr] w-full h-full overflow-hidden rounded-2xl border-2">
-      <aside className="row-span-2 w-[220px] bg-secondary-foreground text-foreground flex flex-col border-r">
-        <div className="flex-1 overflow-y-auto p-4">Aside content</div>
+      <aside className="row-span-2 w-auto max-w-lg bg-secondary-foreground text-foreground flex flex-col border-r">
+        <div className="flex-1 overflow-y-auto p-4">
+          <UserProfile />
+        </div>
         <div className="flex-1 overflow-y-auto p-4">Aside navigation</div>
       </aside>
       <header className="col-start-2 bg-secondary-foreground flex items-center p-6 z-10">
