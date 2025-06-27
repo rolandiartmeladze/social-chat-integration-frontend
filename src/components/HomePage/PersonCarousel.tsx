96fd4a56cd5bd39ff7bd0a3add726568bcd43b44
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-27 04:15:08 -0700

    fix:/ type error

--- src/components/HomePage/PersonCarousel.tsx
+++ src/components/HomePage/PersonCarousel.tsx
@@ -28,23 +28,25 @@ export default function PersonCarousel() {
   return (
     <div className="absolute left-3 md:left-14 bottom-0 md:-bottom-5 md:h-full md:w-1/2 flex items-center justify-start pointer-events-none">
       <AnimatePresence mode="wait">
-        <motion.div
-          key={index}
-          initial={{ opacity: 0, x: -50 }}
-          animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: -50 }}
-          transition={{ duration: 1 }}
-          className="w-[220px] lg:w-[400px] h-auto relative"
-        >
-          <Image
-            src={persons[index]}
-            alt={`Person ${index}`}
-            width={isMobile ? 280 : 400}
-            height={isMobile ? 280 : 400}
-            className={`object-contain ${isMobile ? "w-[80%] h-auto" : "w-[500px]"}`}
-            priority
-          />
-        </motion.div>
+        {typeof persons[index] !== "undefined" ? (
+          <motion.div
+            key={index}
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -50 }}
+            transition={{ duration: 1 }}
+            className="w-[220px] lg:w-[400px] h-auto relative"
+          >
+            <Image
+              src={persons[index]}
+              alt={`Person ${index}`}
+              width={isMobile ? 280 : 400}
+              height={isMobile ? 280 : 400}
+              className={`object-contain ${isMobile ? "w-[80%] h-auto" : "w-[500px]"}`}
+              priority
+            />
+          </motion.div>
+        ) : null}
       </AnimatePresence>
     </div>
   );
