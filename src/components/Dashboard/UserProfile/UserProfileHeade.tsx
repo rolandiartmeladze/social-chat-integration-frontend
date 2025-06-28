e7bd84b3a6555afe3f6ea64f43cd2ad3e30c9dfd
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-27 17:02:18 -0700

    feat:/  create components with  with present user  profile details in dashboart conmonen

---
+++ src/components/Dashboard/UserProfile/UserProfileHeade.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { useAuth } from "@/app/auth/useAuth/useAuth";
+import { Button } from "@/components/ui/button";
+import { Icons } from "@/components/icons";
+import Link from "next/link";
+
+export default function UserProfileHeader() {
+    const { user } = useAuth();
+
+    return (
+        <div className="w-full flex items-center justify-between mb-6 flex-wrap gap-4">
+            <div className="flex items-center gap-4">
+                <Avatar className="size-12">
+                    <AvatarImage src={user?.avatarUrl} className="rounded-lg" />
+                    <AvatarFallback className="text-lg rounded-lg">
+                        {user?.name?.charAt(0) || "U"}
+                    </AvatarFallback>
+                </Avatar>
+                <div className="flex flex-col">
+                    <h1 className="text-xl font-semibold text-foreground">
+                        {user?.name || "User Name"}
+                    </h1>
+                    <p className="text-sm text-muted-foreground">
+                        {user?.email || "user@example.com"}
+                    </p>
+                </div>
+            </div>
+
+            <Button
+                asChild
+                variant="secondary"
+                size="icon"
+                className="relative text-foreground hover:bg-primary"
+            >
+                <Link href="/dashboard/profile/edit">
+                    <Icons.pencil className="size-5 sm:size-6" />
+                </Link>
+            </Button>
+        </div>
+    );
+}
