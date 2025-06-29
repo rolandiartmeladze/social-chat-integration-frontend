763089340fb4cefb39771c6db6bb428edfad811e
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-29 03:02:21 -0700

    ref:/ update structure

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,20 +1,10 @@
 "use client";
-
-import UserProfile from "./UserProfile/UserProfile";
-import AddIntegratonButton from "./AddIntegrationButton";
-import SidebarNavigation from "./SidebarNavigation/SidebarNavigation";
+import DashboardSidebar from "./Sidebar/DashboardSidebar";
 
 export default function Dashboard() {
   return (
-    <div className="grid grid-cols-[auto_1fr] grid-rows-[auto_1fr] w-full h-full overflow-hidden rounded-2xl border-3 dashboard-shadow">      
-    <aside className="row-span-2 p-4 w-auto max-w-lg bg-background/50 text-foreground flex flex-col border-r gap-3">
-      <UserProfile />
-      <SidebarNavigation />
-      <AddIntegratonButton
-        variant="secondary"
-        className="bg-primary/60 hover:bg-primary/20"
-      />
-    </aside>
+    <div className="grid grid-cols-[auto_1fr] grid-rows-[auto_1fr] w-full h-full overflow-hidden rounded-2xl border-3 dashboard-shadow">    
+    <DashboardSidebar />  
       <header className="col-start-2 bg-secondary-foreground flex items-center p-6 z-10">
         <h1 className="text-xl font-bold text-foreground">Dashboard Header</h1>
       </header>
