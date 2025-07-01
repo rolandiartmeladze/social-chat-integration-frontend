4f4c3101375741addfaeba244351b12caf0001a3
Roland Artmeladze <rartmeladze@gmail.com>
2025-07-01 06:05:43 -0700

    ref:/ update dashboard with this change

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,16 +1,13 @@
 "use client";
 import MainContetn from "./MainContent/MainContent";
 import DashboardSidebar from "./Sidebar/DashboardSidebar";
+import DashboardHeader from "./Header/DashboardHeader";
 
 export default function Dashboard() {
   return (
     <div className="grid grid-cols-[auto_1fr] grid-rows-[auto_1fr] w-full h-full overflow-hidden rounded-2xl border-3 dashboard-shadow">
       <DashboardSidebar />
-      <header className="col-start-2 bg-background flex items-center h-20 z-10 border-b">
-        <h1>
-          header
-        </h1>
-      </header>
+      <DashboardHeader />
       <MainContetn />
     </div>
   );
