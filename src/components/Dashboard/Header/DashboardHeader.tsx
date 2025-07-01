af72569906a3bd4296b629f61ddd0d57ca258e1c
Roland Artmeladze <rartmeladze@gmail.com>
2025-07-01 06:05:13 -0700

    feat:/ I created a separate header component for the dashboard that will return navigation elements for the authorized user.

---
+++ src/components/Dashboard/Header/DashboardHeader.tsx
@@ -0,0 +1,10 @@
+export default function dashboardHeader() {
+    return (
+        <header className="col-start-2 bg-background flex items-center h-20 z-10 border-b">
+            <h1>
+                header
+            </h1>
+        </header>
+
+    );
+}
